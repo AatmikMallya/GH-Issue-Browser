e3ea1d7fa464f1dbea68c18366fcfe311e964a14
Aatmik Mallya <aatmik54@gmail.com>
2020-11-11 00:58:20 -0700

    finish project

--- main.py
+++ main.py
@@ -4,25 +4,22 @@ import os
 
 app = Flask(__name__, static_folder='ng/dist/ng', static_url_path='')
 
-
-
+# Route definitions
 @app.route('/')
 def home():
     return make_response(open('ng/dist/ng/index.html').read())
 
+@app.route('/api/issues')
+def get_issues():
+	pass
+
+@app.route('/api/issue_data')
+def get_issue_data():
+	pass
+
 @app.errorhandler(404)
 def not_found_error(error):
     return make_response(open('ng/dist/ng/index.html').read())
-
-@app.route('/images/<image>')
-def get_image(image=None):
-	if os.path.isfile('assets/images/{}'.format(image)):
-		return send_file('assets/images/{}'.format(image))
-	else:
-		abort(404, discription='Image not found')
-
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
